393d7903e3a21866a5f6b6b3341ab64a56b71e09
Ben Casey <ben.r.casey@gmail.com>
2018-07-16 21:40:42 +0100

    Added MySQL

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'forum',
+        'USER': 'forumuser',
+        'PASSWORD': 'bbdbe444288550204c968fe7002a97a9',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
